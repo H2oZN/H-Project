d695940be77bc3f8e51bada9b138e97149be8ea6
H2oZN <Phonlatues@gmail.com>
2025-05-03 20:27:10 +0700

    project Start

---
+++ Components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Header() {
+    return (
+        <div className='text-center bg-gray-500 text-white p-4'>
+            Header
+        </div>
+    )
+}
