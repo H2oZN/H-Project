becba3be653dca1a5e5aafc85452159aafcf14bf
H2oZN <Phonlatues@gmail.com>
2025-05-05 01:15:57 +0700

    finish headder add fade duration

--- Components/CustomButtonHeader.js
+++ Components/CustomButtonHeader.js
@@ -3,7 +3,9 @@ import React from "react";
 export default function CustomButton({ children }) {
   return (
     <button className="relative group">
-      <span className="text-black">{children}</span>
+      <span className="text-gray-400 hover:text-black transition-colors duration-500 ">
+        {children}
+      </span>
       <span className="absolute left-0 bottom-0 w-0 h-[2px] bg-black transition-all duration-500 group-hover:w-full"></span>
     </button>
   );
--- Components/Header.js
+++ Components/Header.js
@@ -3,12 +3,13 @@ import CustomButton from "./CustomButtonHeader";
 
 export default function Header() {
   return (
-    <div className="flex flex-row justify-between p-2  bg-gray-200">
-      <div className="flex items-center mx-2">LOGO</div>
-      <div className="flex items-center flex-row py-5 space-x-10 mx-5">
+    <div className="flex flex-row justify-between p-2 ">
+      <div className="flex items-center mx-2">H2o</div>
+      <div className="flex items-center flex-row py-5 space-x-10 mx-10">
         <CustomButton>Portfolio</CustomButton>
-        <CustomButton>Traveling Experience</CustomButton>
+        <CustomButton>Traveling</CustomButton>
         <CustomButton>My Photo</CustomButton>
+        <CustomButton>Review</CustomButton>
       </div>
     </div>
   );
