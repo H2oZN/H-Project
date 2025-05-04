8c0e537e250255588ffe5f5caff3f92e0cacb705
H2oZN <Phonlatues@gmail.com>
2025-05-04 14:13:28 +0700

    Header Almost Done

--- Components/Header.js
+++ Components/Header.js
@@ -2,9 +2,16 @@ import React from 'react'
 
 export default function Header() {
     return (
-        <div className=' fixed top-0 w-full '>
-            <div className='text-center bg-gray-500 text-white p-2 h-auto text-xs'>This</div>
-            <div className='text-center bg-gray-500 text-white p-2 h-auto text-xs'>That</div>
+        <div className='flex flex-row justify-between bg-gray-200 p-2 '>
+
+            <div className=''>LOGO</div>
+
+            <div className='flex flex-row py-5'>
+                <div className='mx-10'>h1</div>
+                <div className='mx-10'>h2</div>
+                <div className='mx-10'>h3</div>
+                <button className='mx-2'>this button</button>
+            </div>
         </div>
     )
 }
