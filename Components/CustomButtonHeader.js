1d80ce684dc62602e7de210786c9e41339cc82ae
H2oZN <Phonlatues@gmail.com>
2025-05-05 01:25:49 +0700

    set duration

--- Components/CustomButtonHeader.js
+++ Components/CustomButtonHeader.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function CustomButton({ children }) {
   return (
     <button className="relative group">
-      <span className="text-gray-400 hover:text-black transition-colors duration-500 ">
+      <span className="text-gray-500 hover:text-black transition-colors duration-500 ">
         {children}
       </span>
       <span className="absolute left-0 bottom-0 w-0 h-[2px] bg-black transition-all duration-500 group-hover:w-full"></span>
