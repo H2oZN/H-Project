ab869aa937161e70749de4cab4948c5527da47d2
H2oZN <Phonlatues@gmail.com>
2025-05-05 12:56:53 +0700

    commit to save

--- Components/Footer.js
+++ Components/Footer.js
@@ -1,7 +1,11 @@
-import React from 'react';
+import React from "react";
 
 export default function Footer() {
-    return (
-        <div className='fixed bottom-0 w-full bg-gray-200 p-2 text-center'>Footer</div>
-    );
+  return (
+    <div className="fixed bottom-0 w-full flex flex-row justify-between py-4 px-5 bg-gray-200 ">
+      <div>H1</div>
+      <div>H2</div>
+      <div>H3</div>
+    </div>
+  );
 }
--- Components/Header.js
+++ Components/Header.js
@@ -4,7 +4,9 @@ import CustomButton from "./CustomButtonHeader";
 export default function Header() {
   return (
     <div className="flex flex-row justify-between p-2 ">
-      <div className="flex items-center mx-2">H2o</div>
+      <button className="flex items-center mx-2 text-2xl hover:text-red-600 transition-colors duration-500">
+        H2o
+      </button>
       <div className="flex items-center flex-row py-5 space-x-10 mx-10">
         <CustomButton>Portfolio</CustomButton>
         <CustomButton>Traveling</CustomButton>
